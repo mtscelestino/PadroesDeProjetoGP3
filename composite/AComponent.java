a596b2f4250e79e52d835f26cb9bfad6266a3288
Victor Prado <66100225+jvictorprado@users.noreply.github.com>
2021-12-29 19:46:57 -0300

    Component

---
+++ composite/AComponent.java
@@ -0,0 +1,8 @@
+abstract class AComponent{
+  public abstract int getPrice();
+  public abstract String getName();
+  public void add(AComponent item){
+  }
+  public void remove(AComponent item){ 
+  }
+}
